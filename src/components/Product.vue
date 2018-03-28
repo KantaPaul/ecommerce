67d3dc1bab15646b1c51e2f677bdbb9370b34139
Kanta Paul <pobon.paul1994@gmail.com>
2018-03-29 00:10:09 +0600

    finally done

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -4,7 +4,7 @@
         <div class="media align-items-center">
           <a href="#" @click.prevent="isShowingCart = false" class="d-flex">Home</a>
           <div class="media-body d-flex align-items-center justify-content-end">
-            <p class="mb-0 pr-3">{{ cart.length }} <template v-if="cart.lenght == 1">item</template> <template v-else>items</template> in cart, Totalling {{ cartTotal | currency }}</p>
+            <p class="mb-0 pr-3">{{ cart.length }} <template v-if="cart.length == 1">item</template> <template v-else>items</template> in cart, Totalling {{ cartTotal | currency }}</p>
             <button type="button" @click="isShowingCart = true" class="btn btn-info">View Cart</button>
           </div>
         </div>
@@ -15,7 +15,7 @@
             <img class="card-img-top" src="http://via.placeholder.com/400x250/000/fff" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">
-                <router-link to="/">{{ product.title }}</router-link>
+                {{ product.title }}
               </h5>
               <p class="card-text">{{ product.description }}</p>
               <div class="clearfix mb-3">
@@ -33,9 +33,51 @@
           </div>
         </div>
       </div>
-      <div v-else class="text-center">
-        <h1 class="display-1">Cart</h1>
-        <p class="lead">The cart will be display here</p>
+      <div v-else>
+        <h1 class="display-5 text-center">Cart</h1>
+        <table v-if="cart.length > 0" class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Product</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in cart" :key="index">
+              <td><button :class="{disabled : item.quantity > 0}" type="submit" class="btn btn-danger" @click="removeItem(item)">Remove</button> &nbsp; {{ item.product.title }}</td>
+              <td>
+                <button type="submit" @click="itemDecerease(item)" :disabled="item.quantity == 0" class="btn btn-danger">-</button>
+                {{ item.quantity }} &nbsp;
+                <button type="submit" @click="itemIncrease(item)" :disabled="item.product.inStock == 0" class="btn btn-success">+</button>
+              </td>
+              <td>{{ item.quantity * item.product.price | currency }}</td>
+            </tr>
+            <tr>
+              <td class="text-right" colspan="2">
+                <strong>Subtotal</strong>
+              </td>
+              <td>{{ cartTotal | currency }}</td>
+            </tr>
+            <tr>
+              <td class="text-right" colspan="2">
+                <strong>Taxes</strong>
+              </td>
+              <td>{{ taxAmaunt | currency }}</td>
+            </tr>
+            <tr>
+              <td class="text-right" colspan="2">
+                <strong>Grandtotal</strong>
+              </td>
+              <td>{{ grandTotal | currency }}</td>
+            </tr>
+            <tr>
+              <td colspan="2"></td>
+              <td><button type="submit" class="btn btn-primary" @click="checkOut">Check Out</button></td>
+            </tr>
+          </tbody>
+        </table>
+        <p v-else class="text-center display-4 text-danger">Cart is empty</p>
       </div>
   </div>
 </template>
@@ -105,11 +147,46 @@ export default {
   },
   methods: {
     addToCart: function (product) {
-      this.cart.push({
-        product: product,
-        quantity: 1
-      })
+      var cartItem = this.getCartItem(product)
+      if (cartItem != null) {
+        cartItem.quantity++
+      } else {
+        this.cart.push({
+          product: product,
+          quantity: 1
+        })
+      }
       product.inStock--
+    },
+    getCartItem: function (product) {
+      for (var i = 0; i < this.cart.length; i++) {
+        if (this.cart[i].product.id === product.id) {
+          return this.cart[i]
+        }
+      }
+      return null
+    },
+    itemIncrease: function (cartItem) {
+      cartItem.product.inStock--
+      cartItem.quantity++
+    },
+    itemDecerease: function (cartItem) {
+      cartItem.product.inStock++
+      cartItem.quantity--
+    },
+    removeItem: function (cartItem) {
+      var index = this.cart.indexOf(cartItem)
+      if (confirm('Are you remove this product')) {
+        this.cart.splice(index, 1)
+      }
+    },
+    checkOut: function () {
+      if (confirm('Are you buy this product')) {
+        this.cart.forEach(function (item) {
+          item.product.inStock += item.quantity
+        })
+        this.cart = []
+      }
     }
   },
   computed: {
@@ -119,6 +196,12 @@ export default {
         total += item.quantity * item.product.price
       })
       return total
+    },
+    taxAmaunt: function () {
+      return ((this.cartTotal * 10) / 100)
+    },
+    grandTotal: function () {
+      return (this.cartTotal + this.taxAmaunt)
     }
   }
 }
